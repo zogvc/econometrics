d944a408960014bcb9ceda7c7e57bd987d3a4d1d
seho <seho6549@gmail.com>
2024-07-26 03:23:22 +0900

    Update GLS

--- codes/regression.py
+++ codes/regression.py
@@ -1,5 +1,5 @@
 #########################################################
-# Econometric Methods
+# Least Squares Regression
 # © 2024 Seho Jeong
 #########################################################
 
@@ -7,11 +7,14 @@
 import numpy as np
 from numpy.linalg import inv
 
-# Ordinary Least Squares (OLS)
+# ------------------------------------ #
+#     Ordinary Least Squares (OLS)     #
+# ------------------------------------ #
 class OLS:
     def __init__(self, Y, X):
         self.Y = Y
         self.X = X
+        self.n, self.k = X.shape
 
     def coefficients(self):
         """
@@ -28,9 +31,9 @@ class OLS:
         """
         Y = self.Y
         X = self.X
-        β_hat = self.coefficients()
+        β_ols = self.coefficients()
 
-        return Y - X @ β_hat
+        return Y - X @ β_ols
 
     def projection_matrix(self):
         """
@@ -48,17 +51,67 @@ class OLS:
         X = self.X
         return np.eye(len(X)) - X @ inv(X.T @ X) @ X.T
 
-    def r_squared(self):
+    def r_squared(self, adjusted=False):
         """
         Compute the (unadjusted) R-squared (coefficient of determination).
         """
         Y = self.Y
         X = self.X
-        
-        e_hat = ols_residuals(Y, X).reshape(-1, 1)
+
+        e_hat = self.residuals().reshape(-1, 1)
         Y_demeaned = Y - np.mean(Y)
 
-        num = e_hat.T @ e_hat
-        den = Y.T @ Y
+        SSR = e_hat.T @ e_hat
+        SST = Y.T @ Y
 
-        return num / den
+        return float(1 - SSR / SST)
+
+    def leverage_values(self):
+        """
+        Return the leverage values of the model.
+        """
+        return np.diag(self.projection_matrix())
+
+    def coefficients_loo(self):
+        """
+        Estimate the coefficients of leave-one-out regression
+        """
+        n = self.n
+        X = self.X
+        h = self.leverage_values()
+        β_ols = self.coefficients()
+        res = self.residuals()
+
+        β_loo = []
+        
+        for i in range(n):
+            hii = h[i]   # leverage value
+            ei = res[i]  # residual of i-th observation
+            Xi = X[i, :] # i-th observation
+
+            prediction_error = ei / (1 - hii)
+            loo_estimator = β_ols - inv(X.T @ X) @ Xi * prediction_error
+
+            β_loo.append(loo_estimator)
+
+        return np.array(β_loo)
+
+    def influence(self):
+        """
+        Compute a simple diagnostic for influential observations.
+        """
+        h = self.leverage_values()
+        res = self.residuals()
+
+        prediction_errors = res / (1-h)
+
+        return np.max(h * prediction_errors)
+
+
+
+# ------------------------------------- #
+#    Generalized Least Squares (GLS)    #
+# ------------------------------------- #
+class GLS(OLS):
+    def __init__(self):
+        super().__init__(Y, X)
